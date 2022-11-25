be36b33ebdae06ea396b875b60e3439a69c3b12c
Sangwoo Joo <nooni85@naver.com>
2022-11-25 17:57:57 +0900

    feat(compiler): add 'comments' option

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,16 @@
 module.exports = {
   preset: 'ts-jest/presets/js-with-babel-esm',
   testEnvironment: 'node',
+  transform: {
+    // '^.+\\.[tj]sx?$' to process js/ts with `ts-jest`
+    // '^.+\\.m?[tj]sx?$' to process js/ts/mjs/mts with `ts-jest`
+    '^.+\\.tsx?$': [
+      'ts-jest',
+      {
+        tsconfig: {
+          jsx: 'react-jsx',
+        },
+      },
+    ],
+  },
 };
